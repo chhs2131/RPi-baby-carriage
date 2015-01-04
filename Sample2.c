f32930bbebd78d765dcaf528f9210d3f3c1ddf77
FPast <chgy2131@naver.com>
2015-01-04 20:02:38 +0900

    Sample
    여기다 프밍을하면 인터넷에서도 깔금ㅋㅅㅋ

---
+++ Sample2.c
@@ -0,0 +1,27 @@
+http://cafe.naver.com/openrt
+오로카 카페
+
+http://cafe.naver.com/pipc
+라즈베리파이 카페
+
+http://cafe.naver.com/killdoslab
+킬도스랩 카페
+
+http://cafe.naver.com/carroty
+당근이 카페
+
+http://hertaville.com/
+Hertaville
+
+http://www.raspberry-projects.com/pi/
+IBEX
+
+https://github.com/
+Github
+
+https://projects.drogon.net/
+Drogon
+
+http://www.rasplay.org/
+산딸기마을
+

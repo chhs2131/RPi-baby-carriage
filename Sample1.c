cd4e8503a2887d758d4091458b5e37ace52ac653
FPast <chgy2131@naver.com>
2015-01-08 10:58:40 +0900

    Update Sample1.c

--- Sample1.c
+++ Sample1.c
@@ -1,27 +1,150 @@
-http://cafe.naver.com/openrt
-오로카 카페
+/*/////////////////////////////////
+//
+// Project : Rpi + baby carriger Part 1
+// Last Update : 01.03.15
+// Ver : Alpha
+//
+// Here We have "Gas, Temperature&Humidity, Dust, Bluetooth"
+// Other Program Have "Light"
+//
+*//////////////////////////////////
 
-http://cafe.naver.com/pipc
-라즈베리파이 카페
+#include <stdio.h>
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <wiringPiSPI.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
-http://cafe.naver.com/killdoslab
-킬도스랩 카페
+#define CS_MCP3208 6 //GPIO 6
+#define TEMPHUMI 1   //GPIO 1
+#define CHOOSEUART 2 //GPIO 2
 
-http://cafe.naver.com/carroty
-당근이 카페
+#define SPI_CHANNEL 0
+#define SPI_SPEED 1000000 //1MHz
+#define KEY_NUM 9527
+#define MEM_SIZE 1024
 
-http://hertaville.com/
-Hertaville
+int read_mcp3208_adc(unsigned char);
 
-http://www.raspberry-projects.com/pi/
-IBEX
+int main(void)
+{
+	/*/////////////////////////
+	//
+	// Setting Part
+	//
+	*//////////////////////////
 
-https://github.com/
-Github
+	//Set Variable
+	int shm_id;
+	void *shm_addr;
 
-https://projects.drogon.net/
-Drogon
+	int adcChannel = 0;
+	int adcValue = 0;
 
-http://www.rasplay.org/
-산딸기마을
+	int dustPCS = 0;
+	float dustValue = 0;
+	int fd = serialOpen("/dev/ttyAMA0", 9600);
+	char send[] = { 0x11, 0x01, 0x01, 0xED };
+	char respone[15] = { 0, };
 
+	char bluesend[5] = {0,};
+
+	//Set GPIO, SPI, Shareing Variable
+	if (wiringPiSetup() == -1) return 1; //GPIO ON?
+	if (wiringPiSPISetup(SPI_CHANNEL, SPI_SPEED) == -1) return 1; //SPI ON?
+	if ((shm_id = shmget((key_t)KEY_NUM, MEM_SIZE, IPC_CREAT:0666) == -1)) return -1; //Create Share Var?
+	if ((shm_addr = shmat(shm_id, (void *)0, 0)) == -1) return -1; //Can Write Share Var?
+
+	//Set Pin Input or Output
+	pinMode(CS_MCP3208, OUTPUT);
+	pinMode(TEMPHUMI, INPUT);
+	pinMode(CHOOSEUART, OUTPUT);
+
+
+
+	/*/////////////////////////
+	//
+	// Check Senser Part
+	//
+	*//////////////////////////
+	
+	while (1){
+
+		//Gas Senser
+
+		adcValue = read_mcp3208_adc(adcChannel);
+		printf("adcValue = %u\n", adcValue);
+		if (adcValue >= 10000) bluesend[0] = 1;
+		else bluesend[0] = 0;
+		
+
+		//Dust senser
+		
+		digitalWrite(CHOOSEUART, 1) //relay Switch DUST
+		write(fd, send, 4);
+		read(fd, respone, 7);
+	
+		if (respone[0] = 0x16) // Success
+		{
+			dustPCS = respone[3] * 256 * 256 * 256 + respone[4] * 256 * 256 + respone[5] * 256 + respone[6];
+			dustValue = ((float)(dustPCS * 3528)) / 100000;
+			printf(" Now! Dust : %f\n", dustValue);
+			if (dustValue >= 10000) bluesend[1] = 3;
+			else if (dustValue >= 7000) bluesend[1] = 2;
+			else if (dustValue >= 5000) bluesend[1] = 1;
+			else bluesend[1] = 0;
+		}
+		else if (respone[0] = 0x06) // Failed
+		{
+			printf("Hey! I Fail to Receive..\n");
+			continue;
+		}
+		else printf("Can't connected Dust Senser..\n");
+			
+		serialClose(fd);
+		
+
+		//Temp&Humi Senser
+		
+		if (dustValue >= 10000) bluesend[2] = 3; //////
+		
+
+		//Light Senser
+		
+		printf("Light : %d\n", *(volatile int *)shm_addr);
+		if (bluesend[3] < 256) bluesend[3] = *(volatile int *)shm_addr;
+		else bluesend[4] += 1;
+		
+
+		//Bluetooth
+		
+		digitalWrite(CHOOSEUART, 0) //relay Switch Blue
+		write(fd, bluesend, 5);
+		
+
+	}
+	return 0;
+}
+
+
+int read_mcp3208_adc(unsigned char adcChannel)
+{
+  unsigned char buff[3];
+  int adcValue = 0;
+
+  buff[0] = 0x06 | ((adcChannel & 0x07) >> 2);
+  buff[1] = ((adcChannel & 0x07) << 6);
+  buff[2] = 0x00;
+
+  digitalWrite(CS_MCP3208, 0);  // Low : CS Active
+
+  wiringPiSPIDataRW(SPI_CHANNEL, buff, 3);
+
+  buff[1] = 0x0F & buff[1];
+  adcValue = ( buff[1] << 8) | buff[2];
+
+  digitalWrite(CS_MCP3208, 1);  // High : CS Inactive
+
+  return adcValue;
+}
